f807eb22bb24640e8ed612b30879f89131470907
Palawi <bapcoloj@gmail.com>
2026-06-23 19:30:11 +0200

    feat(home): add showreel section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import HeroSection from '$lib/components/sections/HeroSection.svelte';
+	import ShowreelSection from '$lib/components/sections/ShowreelSection.svelte';
 </script>
 
 <main id="main-content">
 	<HeroSection />
+	<ShowreelSection />
 	<span id="formats" class="sr-only"></span>
 </main>
